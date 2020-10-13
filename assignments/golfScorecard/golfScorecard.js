6b6cdbee8627889bfd55c0e374a1e83a3372428d
SirAwesome-5 <70655939+SirAwesome-5@users.noreply.github.com>
2020-10-13 02:26:09 -0400

    Update golfScorecard.js

--- assignments/golfScorecard/golfScorecard.js
+++ assignments/golfScorecard/golfScorecard.js
@@ -19,8 +19,8 @@ let currentScore;
 let parTotal;
 let nextPar;
 let scoreTotal;
-let clearButton = doucment.createElement("BUTTON");
-clearButton.innerHTML = "C";
+// let clearButton = doucment.createElement("BUTTON");
+// clearButton.innerHTML = "C";
 
 for (let i=1; i<19; i++) {
   // assign the entire table row for hole i to a variable, elem
@@ -28,7 +28,7 @@ for (let i=1; i<19; i++) {
   = document.getElementById(i);
   
   // create a clear button for each hole row
-  element[i].children[4].appendChild(clearButton);
+  // element[i].children[4].appendChild(clearButton);
   
   // assign a function to the + button
   element[i].children[4].children[0].onclick 
