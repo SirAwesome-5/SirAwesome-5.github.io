275a56838b92a43f4d08a3ab23bc13ff9585aa7d
SirAwesome-5 <70655939+SirAwesome-5@users.noreply.github.com>
2020-10-19 23:59:25 -0400

    Update golfScorecard.js

--- assignments/golfScorecard/golfScorecard.js
+++ assignments/golfScorecard/golfScorecard.js
@@ -39,9 +39,10 @@ for (let i=1; i<19; i++) {
   element[i].children[4].children[1].onclick 
   = function(){subtract1(element[i], totals[2]);};
   
-  // assign a function to the new clear button
+  /* assign a function to the new clear button
   element[i].children[4].children[2].onlick
   = function(){clearRow(element[i], totals[2]);};
+  */
   
   // compute and display par total
   parTotal = totals[1].innerHTML;
@@ -125,7 +126,7 @@ function overUnderTotal () {
     return totals[3].innerHTML = scoreTotal - parTotal - 4;
 }
 
-// create a "clearRow" function
+/* create a "clearRow" function
 function clearRow (elem, total) {
   // adjust the score total
   let scoreTemp = 0;
@@ -140,3 +141,4 @@ function clearRow (elem, total) {
   totals[3].innerHTML
   = overUnderTotal();
 }
+*/
